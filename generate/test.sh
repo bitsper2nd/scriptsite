d3ad294ceb93bd4831f578b3a97015a0453fe991
bitsper2nd <bitsper2nd@outlook.com>
2023-04-02 22:20:03 -0400

    Added a new filter based user script

--- generate/test.sh
+++ generate/test.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+echo "::group::Testing AP script generator"
+cd ap
+go test -v ./...
+cd ..
+echo "::endgroup::"
+
 
 echo "::group::Testing cosmetic filter script generator"
 cd cosmetic
